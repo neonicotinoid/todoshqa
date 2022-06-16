d920867f2611b0530358f989d7a039e5835a4c27
neonicotinoid <skyyyr@gmail.com>
2022-06-16 16:17:38 +0300

    WIP: Наброски для интерфейса

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,31 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+
+<!DOCTYPE html>
+<html class="h-full bg-gray-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+    </head>
+
+    <body class="h-full">
+        {{ $slot }}
+    </body>
+</html>
