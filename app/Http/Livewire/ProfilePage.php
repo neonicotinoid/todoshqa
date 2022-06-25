a6514a950d8d32c4ebba4154bf5c061409195597
neonicotinoid <skyyyr@gmail.com>
2022-06-25 19:20:12 +0300

    Редактирование профиля и загрузка\удаление аватаров

--- app/Http/Livewire/ProfilePage.php
+++ app/Http/Livewire/ProfilePage.php
@@ -2,29 +2,47 @@
 
 namespace App\Http\Livewire;
 
+use App\Actions\AttachAvatarToUserAction;
 use App\Models\User;
-use Illuminate\Validation\Rule;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules\Password;
-use Illuminate\Validation\Validator;
 use Livewire\Component;
+use Livewire\TemporaryUploadedFile;
+use Livewire\WithFileUploads;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ProfilePage extends Component
 {
 
+    use WithFileUploads;
+
     public User $user;
+    public ?Media $currentAvatar = null;
+    public $preloadedAvatar = null;
     public string $newPassword = '';
     public string $newPassword_confirmation = '';
 
     public function mount(User $user)
     {
         $this->user = $user->makeHidden('password');
+        $this->currentAvatar = $this->user->getFirstMedia('avatar');
+    }
+
+    protected function getListeners()
+    {
+        return [
+//            'avatar-updated' => '$refresh',
+        ];
     }
 
     protected function getRules(): array
     {
         return [
-            'user.name' => ['string', 'required'],
+            'user.name' => ['string', 'required', 'min:3'],
             'user.email' => ['email'],
+            'preloadedAvatar' => ['file', 'image', 'max:1024', 'nullable'],
             'newPassword' => ['required', 'confirmed', Password::min(8)],
         ];
     }
@@ -34,7 +52,7 @@ class ProfilePage extends Component
         if ($this->newPassword === '') {
             // Check without password
             $this->validate([
-                'user.name' => ['string', 'required'],
+                'user.name' => ['string', 'required', 'min:3'],
                 'user.email' => ['email'],
             ]);
         } else {
@@ -44,6 +62,18 @@ class ProfilePage extends Component
         $this->user->save();
     }
 
+    public function removeAvatar()
+    {
+        $this->currentAvatar->delete();
+        $this->currentAvatar = null;
+        $this->preloadedAvatar = null;
+    }
+
+    public function approvePreloadedAvatar(AttachAvatarToUserAction $action)
+    {
+        $this->currentAvatar = $action($this->user, $this->preloadedAvatar);
+        $this->preloadedAvatar = null;
+    }
 
     public function render()
     {
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
     <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-2 py-6 sm:px-6 lg:px-8">
             <livewire:profile-page :user="auth()->user()" />
         </div>
     </main>
