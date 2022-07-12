e71f48600e10245a47570ba888888d73afd33702
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 <skyyyr@gmail.com>
2022-07-12 16:32:33 +0300

    WIP: Цвета инпутов

--- resources/js/components/Form/TextInput.vue
+++ resources/js/components/Form/TextInput.vue
@@ -3,9 +3,9 @@
            :value="modelValue"
            @input="$emit('update:modelValue', $event.target.value)"
     class="block mt-1.5 w-full px-2.5 py-2 rounded-lg outline-none border border-gray-300 shadow-sm
-                hover:border-purple-300
+                hover:border-blue-300
                 placeholder:text-gray-500 placeholder:font-normal
-                focus:ring-1 focus:ring-purple-500 focus:border-purple-500 ring-inset
+                focus:ring-1 focus:ring-blue-500 focus:border-blue-500 ring-inset
                 disabled:bg-gray-200 disabled:text-white disabled:cursor-not-allowed disabled:hover:border-gray-300
                 duration-150"
            :class="{'ring-1 ring-red-500 border-red-500' : isError} "
--- resources/js/components/Form/TextareaInput.vue
+++ resources/js/components/Form/TextareaInput.vue
@@ -2,9 +2,9 @@
     <textarea :value="modelValue"
               @input="$emit('update:modelValue', $event.target.value)"
                 class="block mt-2 w-full px-3 py-2.5 rounded-lg outline-none border border-gray-300 shadow-sm
-                    hover:border-purple-300
+                    hover:border-blue-300
                     placeholder:text-gray-400 placeholder:font-normal
-                    focus:ring-1 focus:ring-purple-500 focus:border-purple-500 ring-inset
+                    focus:ring-1 focus:ring-blue-500 focus:border-blue-500 ring-inset
                     disabled:bg-gray-200 disabled:text-white disabled:cursor-not-allowed disabled:hover:border-gray-300
                     duration-150"
               :class="{'ring-1 ring-red-500 border-red-500' : isError} "
